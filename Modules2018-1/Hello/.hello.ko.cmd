14e54c6af1a9902bdae8bc792ee001307a7e220a
AnnBenavides <AnnBenavides@github.com>
2018-08-11 19:05:20 -0400

    vigia-impl based on pipe-impl

---
+++ Modules2018-1/Hello/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/bastyz/SOt3/Modules2018-1/Hello/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/bastyz/SOt3/Modules2018-1/Hello/hello.ko /home/bastyz/SOt3/Modules2018-1/Hello/hello.o /home/bastyz/SOt3/Modules2018-1/Hello/hello.mod.o ;  true
---
+++ Modules2018-1/Hello/.tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/bastyz/SOt3/Modules2018-1/Hello/hello.ko
+/home/bastyz/SOt3/Modules2018-1/Hello/hello.o
+
